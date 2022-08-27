19d7c06e3ed0f09dfbe0c48b4f2f4aef34c359ca
Shreyans <shreyanssdeshmukh21@gmail.com>
2022-08-27 16:25:58 +0530

    initial commit

---
+++ rn-starter-main/src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, StyleSheet, Button, View } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello This is Darshan Here !</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
