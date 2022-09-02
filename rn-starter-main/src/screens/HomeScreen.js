8da0f428d96cb0f754e47408b1a168016bde9846
Shreyans <shreyanssdeshmukh21@gmail.com>
2022-09-02 12:25:06 +0530

    <food>

--- rn-starter-main/src/screens/HomeScreen.js
+++ rn-starter-main/src/screens/HomeScreen.js
@@ -26,6 +26,9 @@ const HomeScreen = ({ navigation }) => {
       <Button
         onPress={() => navigation.navigate('Text')}
         title="Go to text Demo" />
+      <Button
+        onPress={() => navigation.navigate('Box')}
+        title="Box Screen" />
     </View >
 
   );
